0381c0e6b3ac16212cbfcf9989d5055cea8168e8
Kingsley Macauley <kingsmacakn@gmail.com>
2026-08-28 13:40:33 +0100

    Devotional Push 6

--- src/data/devotionals.ts
+++ src/data/devotionals.ts
@@ -284,7 +284,7 @@ function pdfsFor(year: number): DevotionalPdf[] {
         month: "long",
         year: "numeric",
       }),
-      file: `/devotional-pdfs/${year}/Awake-Devotional-${monthName}-${year}.pdf`,
+      file: `https://raw.githubusercontent.com/kingsmac1/flc-usa-website/main/public/devotional-pdfs/${year}/Awake-Devotional-${monthName}-${year}.pdf`,
       available: AVAILABLE_PDF_MONTHS.has(month),
     };
   });
